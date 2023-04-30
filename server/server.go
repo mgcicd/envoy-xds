e149da723907cb432beb17cd41d7c255f362d058
0000355 <pijiu@suiyi.com.cn>
2023-04-30 10:33:21 +0800

    grpc通信使用证书加密

--- server/server.go
+++ server/server.go
@@ -2,18 +2,42 @@ package server
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"net"
 
 	sdsV3 "github.com/envoyproxy/go-control-plane/envoy/service/secret/v3"
 	"github.com/mgcicd/cicd-core/logs"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func NewXdsServer(port int) {
 	ctx := context.Background()
 
-	server := grpc.NewServer()
+	cert, err := tls.LoadX509KeyPair("certs/server.pem", "certs/server.key")
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	rootBuf, err := ioutil.ReadFile("certs/ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	if !certPool.AppendCertsFromPEM(rootBuf) {
+		panic("Fail to append ca")
+	}
+
+	tlsConf := &tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    certPool,
+	}
+
+	grpcServer := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConf)))
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
 
@@ -23,17 +47,17 @@ func NewXdsServer(port int) {
 
 	serverHandle := &SecretDiscoveryService{}
 	//注册sds
-	sdsV3.RegisterSecretDiscoveryServiceServer(server, serverHandle)
+	sdsV3.RegisterSecretDiscoveryServiceServer(grpcServer, serverHandle)
 
 	logs.DefaultConsoleLog.Info("NewXdsServer", fmt.Sprintf("xds GRPC Server Listening On %v", port))
 
 	go func() {
-		if err = server.Serve(lis); err != nil {
+		if err = grpcServer.Serve(lis); err != nil {
 			logs.DefaultConsoleLog.Error("NewXdsServer", fmt.Sprintf("err : %s", err))
 		}
 	}()
 
 	<-ctx.Done()
 
-	server.GracefulStop()
+	grpcServer.GracefulStop()
 }
